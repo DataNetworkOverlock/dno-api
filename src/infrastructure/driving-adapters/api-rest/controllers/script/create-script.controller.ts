651e539b162e86af4b8ea1797f45bba7598bc0f0
miklegonza <gonzalez.michael1803@gmail.com>
2024-01-09 01:25:04 -0500

    :sparkles: DNO-010: Added autentication methods with JSON Web Tokens

--- src/infrastructure/driving-adapters/api-rest/controllers/script/create-script.controller.ts
+++ src/infrastructure/driving-adapters/api-rest/controllers/script/create-script.controller.ts
@@ -1,16 +1,22 @@
 import { CreateScriptUseCase } from '@application/use-cases/create-script';
+import { NotAuthenticatedException } from '@domain/exceptions';
+import { JWTHandlerImpl } from '@infrastructure/driven-adapters/JWT';
 import { UuidV4Generator } from '@infrastructure/driven-adapters/UuidV4';
 import { MySQLScriptRepository } from '@infrastructure/implementations/mysql/MySQLScriptRepository';
 import { NextFunction, Request, Response } from 'express';
 
 export const createScript = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const { name, description, source, tags } = req.body;
-    
+
     const mySQLScriptRepository = new MySQLScriptRepository();
     const uuidV4Generator = new UuidV4Generator();
+    const jwtHandler = new JWTHandlerImpl();
     const createScriptUseCase = new CreateScriptUseCase(mySQLScriptRepository, uuidV4Generator);
 
     try {
+        const accessToken = req.headers['authorization'];
+        const isValid = jwtHandler.validateAccessToken(accessToken);
+        if (!isValid) throw new NotAuthenticatedException();
         const scriptCreated = await createScriptUseCase.run({
             name,
             description,
--- src/infrastructure/driving-adapters/api-rest/controllers/test/create-test.controller.ts
+++ src/infrastructure/driving-adapters/api-rest/controllers/test/create-test.controller.ts
@@ -2,15 +2,21 @@ import { NextFunction, Request, Response } from 'express';
 import { CreateTestUseCase } from '@application/use-cases/create-test';
 import { MySQLTestRepository } from '@infrastructure/implementations/mysql/MySQLTestRepository';
 import { UuidV4Generator } from '@infrastructure/driven-adapters/UuidV4';
+import { JWTHandlerImpl } from '@infrastructure/driven-adapters/JWT';
+import { NotAuthenticatedException } from '@domain/exceptions';
 
 export const createTest = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const { ip, date, report, userId, scriptId } = req.body;
 
     const mysqlTestRepository = new MySQLTestRepository();
     const uuidV4Generator = new UuidV4Generator();
+    const jwtHandler = new JWTHandlerImpl();
     const createTestUseCase = new CreateTestUseCase(mysqlTestRepository, uuidV4Generator);
 
     try {
+        const accessToken = req.headers['authorization'];
+        const isValid = jwtHandler.validateAccessToken(accessToken);
+        if (!isValid) throw new NotAuthenticatedException();
         const testCreated = await createTestUseCase.run({
             ip,
             date,
